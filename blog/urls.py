69ee80bea1d281ffd531070fbf682bacf43484cd
cudjoeab <cudjoeab@gmail.com>
2019-08-05 12:46:00 -0400

    CRUD methods for article and comments are in, need to debug to properly render requests"

--- blog/urls.py
+++ blog/urls.py
@@ -24,24 +24,14 @@ urlpatterns = [
     path('home/', views.home_page, name='home_page'),
     path('article/<int:id>', views.show_article, name='full_article'),
     path('article/<int:id>/edit', views.edit_article, name='edit_article'),
-    # path('article/<int:id>/update', views.update_article, name='update_article'),
+    path('article/<int:id>/update', views.update_article, name='update_article'),
     path('article/new', views.new_article, name='new_article'), 
     path('article/create', views.create_article, name='create_article'),
-    # path('article/<int:id>/delete', views.delete_product, name='delete_product'),
+    path('article/<int:id>/delete', views.delete_article, name='delete_article'),
     # #comment paths#
-    # path(
-    #     "article/<int:product_id>/comments/create",
-    #     views.create_comment,
-    #     name="create_comment",
-    # ),
-
-
-    # path("article/<int:product_id>/comments/<int:comment_id>/update", views.update_comment, name="update_comment"), 
-
-
-    # path("article/<int:product_id>/comments/<int:comment_id>/edit", views.edit_comment, name="edit_comment"), 
-
-    # path("article/<int:product_id>/comments/<int:comment_id>/delete", views.delete_comment, name="delete_comment"), 
+    path("article/<int:product_id>/comments/<int:comment_id>/update", views.update_comment, name="update_comment"), 
+    path("article/<int:product_id>/comments/<int:comment_id>/edit", views.edit_comment, name="edit_comment"), 
+    path("article/<int:product_id>/comments/<int:comment_id>/delete", views.delete_comment, name="delete_comment"), 
 
     
 ]
--- blog/views.py
+++ blog/views.py
@@ -8,8 +8,6 @@ def root(request):
     return HttpResponseRedirect('/home')
 
 def home_page(request):
-    # context = {}
-
     context = { 
     'articles': Article.objects.all().order_by('-published_date'),
     'current_time': datetime.now() 
@@ -21,7 +19,7 @@ def show_article(request, id):
     article = Article.objects.get(pk=id)
     form = CommentForm()
     context = {'article': article, 'form': form}
-    return render(request, 'show.html', context)
+    return render(request, 'article.html', context)
 
 def new_article(request):
     form = ArticleForm()
@@ -37,13 +35,29 @@ def create_article(request): #saving article to database
             return render(request, 'index.html', context)
         else:
             context = {"form": form}
-            return render(request, 'index.html', context)
+            return render(request, 'create_article.html', context)
 
 def edit_article(request, id):  # Renders a form to edit an existing article.
-    article =Article.objects.get(pk=id)
-    form =ArticleForm(instance=article)
+    article = Article.objects.get(pk=id)
+    form = ArticleForm(instance=article)
     context = {"product":article, "form": form}
-    return render(request, "edit_product_form.html", context)
+    return render(request, "edit_article_form.html", context)
+
+def update_article(request, id):  # User updating an existing product.
+    article = Article.objects.get(pk=id)
+    form = ArticleForm(request.POST, instance=article)
+    context = {"article": article, "form": form}
+    if form.is_valid():
+        form.save()
+        return render(request, 'index.html', context)
+    else:  # Else sends user back to existing product form.
+        context = {"article": article, "form": form}
+        return render(request, "edit_article_form.html", context)
+
+def delete_article(request, id):  # User deleting an existing product.
+    article = Article.objects.get(pk=id)
+    article.delete()
+    return redirect(reverse("home_page"))
 
 def create_comment(request):
     article_id = request.POST['article']
@@ -54,5 +68,27 @@ def create_comment(request):
     context = {"form": form, "message": "Add a comment.", "action": "/comments/create", "article": article}
     form.save()
     form = CommentForm()
-    return render(request, 'show.html', context)
+    return render(request, 'article.html', context)
 
+def edit_comment(request, article_id, comment_id):
+    article = Article.objects.get(pk=product_id)
+    comment = Review.objects.get(pk=product_id)
+    comment.article_id = comment_id
+    form = CommentForm(instance=comment)
+    context = {"comment": comment, "form": form, "article": article}
+    return render(request, "edit_comment_form.html", context)
+
+def update_comment(request, article_id, comment_id): 
+    comment = Comment.objects.get(pk=review_id)
+    form = CommentForm(request.POST, instance=comment)
+    if form.is_valid():
+        form.save()
+        return render(request, 'article.html', context)
+    else: 
+        context = {"comment": comment, "form": form, "product": product}
+        return render(request, "edit_comment_form.html", context)
+
+def delete_comment(request, article_id, comment_id):
+    comment = Comment.objects.get(pk=comment_id)
+    comment.delete()
+    return redirect(reverse("article.html", kwargs={"id":article_id}))
